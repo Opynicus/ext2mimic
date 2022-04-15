537ef17622d273fe7fdb0bad3601dad32b578278
ZTLPluz <zenguncle@outlook.com>
2022-04-15 21:27:29 +0800

    fix: redefine some params

--- params.h
+++ params.h
@@ -13,22 +13,22 @@
 using namespace std;
 
 #define IMAGE_FILE_NAME "disk.img"
-#define MAX_FILE_NAME 26
+#define MAX_FILE_NAME 28
 
 #define INODE_NUM 640                                   // inode总数640个
 #define SUPERBLOCK_SIZE 1024                            // superblock大小（byte）
 #define INODE_SIZE 128                                  // inode大小（byte）128B
 #define BLOCK_SIZE 512                                 // block大小（byte), 512B
-#define BLOCK_NUM 8192                                 // 块号数为8192,大小为 8192 * 512B
+#define BLOCK_NUM 10240                                 // 块号数为8192,大小为 8192 * 512B
 
 #define MAX_FREE_BLOCKS 64                              // 空闲块堆栈大小，一个空闲堆栈最多能存多少个磁盘块地址
 
 #define BLOCK_NUM_PER_INODE (INODE_NUM/(BLOCK_SIZE/INODE_SIZE))  //每个INODE节点占的磁盘块数(160)
 
 #define SUPERBLOCK_START_ADDR 0                         // 超级块 偏移地址,占一个磁盘块
-#define BLOCK_BITMAP_START_ADDR (1*BLOCK_SIZE)          // block位图 偏移地址，占8个磁盘块，最多监控 512 * 8 个磁盘块的状态
-#define INODE_BITMAP_START_ADDR (BLOCK_BITMAP_START_ADDR+ 8 * BLOCK_SIZE)    // inode位图 偏移地址，占2个磁盘块，最多监控 512 * 2 个inode的状态
-#define INODE_TABLE_START_ADDR (INODE_BITMAP_START_ADDR+ 2 * BLOCK_SIZE)    // inode节点区 偏移地址，占 BLOCK_NUM_PER_INODE 个磁盘块
+#define INODE_BITMAP_START_ADDR (1*BLOCK_SIZE)          // block位图 偏移地址，占20个磁盘块，最多监控 512 * 20 个磁盘块的状态
+#define BLOCK_BITMAP_START_ADDR (INODE_BITMAP_START_ADDR+ 2 * BLOCK_SIZE)    // inode位图 偏移地址，占2个磁盘块，最多监控 512 * 2 个inode的状态
+#define INODE_TABLE_START_ADDR (INODE_BITMAP_START_ADDR+ 20 * BLOCK_SIZE)    // inode节点区 偏移地址，占 BLOCK_NUM_PER_INODE 个磁盘块
 #define DATA_BLOCK_START_ADDR (INODE_TABLE_START_ADDR + BLOCK_NUM_PER_INODE * BLOCK_SIZE)//block数据区 偏移地址 ，占 INODE_NUM 个磁盘块
 
 #define Dir_ITEM_NUM_PER_BLOCK (BLOCK_SIZE / 32 )
@@ -52,5 +52,6 @@ using namespace std;
 typedef unsigned int inodeid_t;
 typedef unsigned int blockid_t;
 
+#define ROOT_DIR_ADDR INODE_TABLE_START_ADDR
 
 #endif //EXT2MIMIC_PARAMS_H
