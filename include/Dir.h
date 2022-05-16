089d63447f370306a78975c43c91a5303d20f73c
ZTLPluz <zenguncle@outlook.com>
2022-05-16 15:57:12 +0800

    perf: class 'Dir' changed to struct 'Dir'

--- include/Dir.h
+++ include/Dir.h
@@ -7,10 +7,9 @@
 
 #include "params.h"
 
-class Dir {
- public:
+typedef struct Dir { //本质是地址位与名称的Hash Map
   int inodeAddr;
   char file_name[MAX_FILE_NAME];
-};
+} Dir;
 
 #endif //EXT2MIMIC_DIR_H
--- src/Dir.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Opynicus on 2022/4/14.
-//
-
-#include "../include/Dir.h"
